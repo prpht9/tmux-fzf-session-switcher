a086d548ba59f99e0b4d3a7ccdc4a11de9ccc28b
Chris Scheper <prpht9@gmail.com>
2023-07-10 12:57:49 -0400

    fix but with tmux-git install script
     * fix the broken dir test

--- tmux-git-cli-install.sh
+++ tmux-git-cli-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-[ ! -d $HOME/bin ] mkdir $HOME/bin
+[[ ! -d $HOME/bin ]] && mkdir $HOME/bin
 ln -s "$CURRENT_DIR/scripts/tmux-git" $HOME/bin/tmux-git
